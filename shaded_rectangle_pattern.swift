a920260a6c04c998b40cff61c3f727597fae1722
Rohan Sakhare <126242412+rohsak5649@users.noreply.github.com>
2025-01-09 18:32:59 +0530

    Update shaded_rectangle_pattern.swift

--- shaded_rectangle_pattern.swift
+++ shaded_rectangle_pattern.swift
@@ -5,3 +5,7 @@ for i in 0..<4
     }
     print("\n")
 }
+
+//1. for i in 0.. <4: Loops from 0 to 3.
+//2. terminator: " ": Ensures that print does not add a newline after printing * .
+//3. print("'"') : Prints a new line after completing a row.
